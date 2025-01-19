6c4d99c1d65792bdf3677759a08a4dbf349fe589
Tomáš Snětivý <jo2ker@seznam.cz>
2025-01-19 19:42:32 +0100

    Poupravena vizualizace kódu

--- projekt_1.py
+++ projekt_1.py
@@ -5,6 +5,7 @@ autor: Tomáš Snětivý
 email: jo2ker@seznam.cz
 """
 # proměnná TEXTS
+
 TEXTS = ['''
 Situated about 10 miles west of Kemmerer,
 Fossil Butte is a ruggedly impressive
@@ -34,6 +35,7 @@ garpike and stingray are also present.'''
 ]
 
 # slovník reg_users
+
 reg_users = {
     "bob": "123",
     "ann": "pass123",
@@ -42,14 +44,17 @@ reg_users = {
 }
 
 # přihlašovací jméno a heslo
+
 jmeno = input("Zadejte uživatelské jméno: ")
 heslo = input("Zadejte heslo: ")
 
 # oddělovací linka
+
 linka = ("-" * 50)
 print(linka)
 
 # ověření uživatelského jména a hesla
+
 if jmeno in reg_users and reg_users[jmeno] == heslo:
     print(f"Vítej v aplikaci", jmeno, ", máme tři texty k analýze.")
     print(linka)
@@ -58,9 +63,11 @@ else:
     exit()
 
 # uživatelský vstup pro výběr indexu
+
 vybrany_index = input("Vyber číslo textu od 1 do 3, který chceš analyzovat: ")
 
 # kontrola platnosti indexu a zda je vstup číslo
+
 if vybrany_index.isdigit():
     vybrany_index = int(vybrany_index) - 1
     if vybrany_index < 0 or vybrany_index >= len(TEXTS):
@@ -73,40 +80,48 @@ else:
     exit()
 
 # počet slov v textu
+
 slova = TEXTS[vybrany_index].strip().split()
 pocet_slov = len(slova)
 print("Vybraný text obsahuje", pocet_slov, "slov.")
 
 # počet slov začínajících velkým písmenem
+
 velky_pismeno = [_ for _ in slova if _[0].isupper()]
 pocet_velky_pismeno = len(velky_pismeno)
 print("Z toho", pocet_velky_pismeno, "slov, začínajících velkým písmenem.")
 
 # počet slov psaných velkými písmeny
+
 velka_pismena = [_ for _ in slova if _.isupper() and _.isalpha()]
 pocet_velka_pismena = len(velka_pismena)
 print(pocet_velka_pismena, "slov psaných velkými písmeny.")
 
 # počet slov psaných malými písmeny
+
 mala_pismena = [_ for _ in slova if _.islower()]
 pocet_mala_pismena = len(mala_pismena)
 print(pocet_mala_pismena, "slov psaných malými písmeny.")
 
 # počet čísel
+
 cisla = [int(_) for _ in slova if _.isdigit()]
 pocet_cisel = len(cisla)
 print("Obsahuje", pocet_cisel, "číselné řetězce.")
 
 # suma všech čísel v textu
+
 soucet = sum(cisla)
 print("Součet všech čísel v textu je", soucet)
 print(linka)
 
 # výpis hlavičky grafu
+
 print("DÉLKA|       VÝSKYT       |POČET")
 print(linka)
 
 # Vytvoření seznamu délek a četnosti slov
+
 delka_slov = []
 cetnost_delky = []
 
@@ -125,3 +140,4 @@ for delka in delka_slov:
 for i in range(len(cetnost_delky)):
     if cetnost_delky[i] > 0:
         print(f" {i + 1:>4}|{"*" * cetnost_delky[i]:<20}|{cetnost_delky[i]}")
+exit()
